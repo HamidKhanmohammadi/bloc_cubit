9a8a42a5b17c9b7bbbfcee8738da598a6670b98e
H.Khanmohammadi <h.khanmohammadi.hamid@gmail.com>
2024-01-14 12:47:58 +0330

    blocProvider and blocBuilder

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,33 +1,50 @@
+import 'package:bloc_cubit/bloc/sounter_cubit.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("2", style: TextStyle(fontSize: 20),),
-            SizedBox(height: 20,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(onPressed: () {
-
-                }, child: Text("increase")),
-                SizedBox(width: 20,),
-                ElevatedButton(onPressed: () {
-
-                }, child: Text("increase")),
-              ],
-            )
-          ],
-        ),
-      ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              BlocBuilder<CounterCubit, int>(
+                builder: (context, state) {
+                  return Directionality(
+                    textDirection: TextDirection.ltr,
+                      child: Text(state.toString(), style: TextStyle(fontSize: 20),));
+                },
+              ),
+              SizedBox(height: 20,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(onPressed: () {
+                    BlocProvider.of<CounterCubit>(context).increase();
+                  }, child: Directionality(
+                      textDirection: TextDirection.ltr,
+                      child: Text("increase"))),
+                  SizedBox(width: 20,),
+                  ElevatedButton(onPressed: () {
+                    BlocProvider.of<CounterCubit>(context).decrease();
+                  }, child: Directionality(
+                      textDirection: TextDirection.ltr,
+                      child: Text("decrease"))),
+                ],
+              )
+            ],
+          ),
+        )
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,32 @@
+import 'package:bloc_cubit/bloc/sounter_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'home_page.dart';
 
 void main() {
-  runApp(const HomePage());
+  runApp(MaterialApp(home: MyApp()));
 }
 
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with TickerProviderStateMixin {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => CounterCubit(),
+      child: const HomePage(),
+    );
+  }
+}
